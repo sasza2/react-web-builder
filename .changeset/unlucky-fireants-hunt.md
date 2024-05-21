abbbfde804007198e2ea1e332ae04ade3e4655ec
sasza <sasza@onet.eu>
2024-05-21 22:18:40 +0200

    fix: go back to grid boundary when removing element that is at the bottom of grid

---
+++ .changeset/unlucky-fireants-hunt.md
@@ -0,0 +1,5 @@
+---
+"react-web-builder": patch
+---
+
+go back to grid boundary when removing element that is at the bottom of grid
--- src/hooks/useRemoveElement.ts
+++ src/hooks/useRemoveElement.ts
@@ -1,12 +1,17 @@
+import { useGridAPI } from '@/components/GridAPIProvider';
 import { removeElementFromBreakpoint } from '@/store/elementsInBreakpointsSlice';
 import { useAppDispatch } from '@/store/useAppDispatch';
+import { delay } from '@/utils/delay';
 import { useBreakpoint } from './useBreakpoint';
 
 export const useRemoveElement = () => {
   const dispatch = useAppDispatch();
   const breakpoint = useBreakpoint();
+  const gridAPIRef = useGridAPI();
 
-  return (elementId: string | number) => {
+  return async (elementId: string | number) => {
     dispatch(removeElementFromBreakpoint({ elementId, breakpointId: breakpoint.id }));
+    await delay(200)
+    gridAPIRef.current.getPanZoom().goBackToBoundary()
   };
 };
