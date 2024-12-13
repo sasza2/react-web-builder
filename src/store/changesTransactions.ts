901e4f07a876d92c91344cb6002c68542138502e
sasza <sasza@onet.eu>
2024-12-13 19:47:24 +0100

    lint fixes

--- src/store/changesTransactions.ts
+++ src/store/changesTransactions.ts
@@ -1,7 +1,13 @@
+import { Action } from '@reduxjs/toolkit';
+
 export const CHANGES_START_TRANSACTION_TYPE = 'changesStartTransaction';
 
 export const CHANGES_STOP_TRANSACTION_TYPE = 'changesStopTransaction';
 
-export const changesStartTransaction = () => ({ type: CHANGES_START_TRANSACTION_TYPE });
+export const changesStartTransaction = (): Action<typeof CHANGES_START_TRANSACTION_TYPE> => ({
+  type: CHANGES_START_TRANSACTION_TYPE,
+});
 
-export const changesStopTransaction = () => ({ type: CHANGES_STOP_TRANSACTION_TYPE });
+export const changesStopTransaction = (): Action<typeof CHANGES_STOP_TRANSACTION_TYPE> => ({
+  type: CHANGES_STOP_TRANSACTION_TYPE,
+});
